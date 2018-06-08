d4a158e294013176d92e99e3d95b1845604e69cb
Ivan Stanishev <istanishev@cimpress.com>
2018-06-08 14:26:48 +0200

    Fix build pipeline

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,24 +27,10 @@ module.exports = function(grunt) {
       },
       target: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js'],
     },
-    'babel': {
-      options: {
-        sourceMap: true,
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'src/',
-          src: ['*.js'],
-          dest: 'dist/',
-        }],
-      },
-    },
   });
 
   grunt.loadNpmTasks('grunt-mocha-istanbul');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-babel');
 
   grunt.registerTask('default', ['eslint']);
   grunt.registerTask('test', ['default', 'mocha_istanbul:coverage', 'istanbul_check_coverage']);
